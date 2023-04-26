5c83b5ca8c03301814b56e407d594d21fdb186b4
Amit Patel <120770473+AmitPatel-analyst@users.noreply.github.com>
2023-04-26 16:17:44 +0530

    Create Danny's Diner Solution.sql

---
+++ #8Weeksqlchallange/Case Study # 1 - Danny's Diner/Danny's Diner Solution.sql
@@ -0,0 +1,13 @@
+----------------------------------
+-- CASE STUDY #1: DANNY'S DINER --
+----------------------------------
+
+-- Author: Amit Patel
+-- Date: 25/04/2023 
+-- Tool used: MSSQL Server
+
+--------------------------
+-- CASE STUDY QUESTIONS --
+--------------------------
+
+-- 1. What is the total amount each customer spent at the restaurant?
