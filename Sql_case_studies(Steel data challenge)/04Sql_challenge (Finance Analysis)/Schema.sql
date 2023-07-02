43061c4d031bb46cd65a44efd16600a8d6e3e270
Amit Patel <120770473+AmitPatel-analyst@users.noreply.github.com>
2023-07-02 10:04:45 +0530

    Added Date

--- Sql_case_studies(Steel data challenge)/04Sql_challenge (Finance Analysis)/Schema.sql
+++ Sql_case_studies(Steel data challenge)/04Sql_challenge (Finance Analysis)/Schema.sql
@@ -1,7 +1,7 @@
 /*****************************************************************************************
 Name	: Code for Steel_Data DB
 Author	: Amit Patel
-
+Date   : 12-May-2023
 
 Purpose: This script will create Db and few table in it to store info about Finance Analysis
 *****************************************************************************************/
